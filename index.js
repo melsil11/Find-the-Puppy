000f0bf2c0161be9689ee881a982ae3cda6df9a8
melanie silva <melsil11@hotmail.com>
2022-09-21 13:05:32 -0400

    added randomizer still working on css

--- index.js
+++ index.js
@@ -8,6 +8,64 @@ const computer = {
 
 const choices = ['faceCard1', 'faceCard2', 'faceCard3'];
 
+function shuffleArray(choices) {
+    for (let i = choices.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [choices[i], choices[j]] = [choices[j], choices[i]];
+    }
+  console.log(choices);
+  }
+
+  shuffleArray(choices);
+// console.log(choices)
+
+
+function computerChooses(){
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    computer.currentChoice = choices[randomIndex];
+    }
+
+    function compareChoices() {
+        player.currentChoice = event.currentTarget.innerText 
+         computerChooses();
+         
+         if(computer.currentChoice === player.currentChoice) {
+           document.getElementById("results").innerText="It's a tie. The computer and player both chose " + computer.currentChoice;
+       }else if(computer.currentChoice === choices[0]){
+         if(player.currentChoice === choices[1]){
+           document.getElementById("results)".innerText="The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice);
+         }else{
+           document.getElementById("results").innerText="The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+         }
+       }else if(computer.currentChoice === choices[1]){
+          if(player.currentChoice === choices[2]){
+            document.getElementById("results").innerText="The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+       }else{
+         document.getElementById("results").innerText="The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+         }
+       }else if(computer.currentChoice === choices[2]){
+           if(player.currentChoice === choices[0]){
+             document.getElementById("results").innerText="The player wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+         }else{
+           document.getElementById("results").innerText="The computer wins! The computer chose " + computer.currentChoice + " and the player chose " + player.currentChoice;
+         }
+        }
+       }
+          
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // console.log (choices)
@@ -31,25 +89,23 @@ const choices = ['faceCard1', 'faceCard2', 'faceCard3'];
 //  };
 
 
-function computerChooses() {
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    computer.currentChoice = choices[randomIndex];
-}
+// function computerChooses() {
+//     const randomIndex = Math.floor(Math.random() * choices.length);
+//     computer.currentChoice = choices[randomIndex];
+// }
 
-function compareChoices() {
-    player.currentChoice = event.currentTarget.innerText
-    computerChooses();
-
-
-}
+// function compareChoices() {
+//     player.currentChoice = event.currentTarget.innerText
+//     computerChooses();
 
 
+// }
+
+
+document.getElementById('faceCard1').addEventListener('click', compareChoices);
+document.getElementById('faceCard2').addEventListener('click', compareChoices);
+document.getElementById('faceCard3').addEventListener('click', compareChoices);
+// document.querySelector('.restart-game').addEventListener('click', handleRestartGame);
 
 
 
-
-
-document.getElementById('faceCard1').addEventListener('click', compareChoices)
-document.getElementById('faceCard2').addEventListener('click', compareChoices)
-document.getElementById('faceCard3').addEventListener('click', compareChoices)
-document.querySelector('.restart-game').addEventListener('click', handleRestartGame);
