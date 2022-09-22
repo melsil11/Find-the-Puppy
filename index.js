d72c9532490a17a6892fbb0b342f1b230213fa76
melanie silva <melsil11@hotmail.com>
2022-09-22 15:43:14 -0400

    fixed the match issue by being specific in console.logs and changing the matchCheck function for lecetion ===facecard1

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ const computer = {
 
 
 const matchCard = ['matchCard']
+// the array that will be shuffled and used to match the choices in the game///
 const choices = ['faceCard1', 'faceCard2', 'faceCard3'];
-// const cardChoices = getElementsByClassName('cards')
+
 
 // console.log(getElementsByClassName('cards'))
-const choice2 = document.createElement('div')
-    choice2.setAttribute('id', 'fisher') 
-    document.body.appendChild(choice2);
+// const choice2 = document.createElement('div')
+//     choice2.setAttribute('id', 'fisher') 
+//     document.body.appendChild(choice2);
     // const fisher = document.createElement('img');
     // fisher.src = 'images/fisher.jpg';
     // document.body.appendChild(fisher);
@@ -32,54 +33,86 @@ function shuffleArray(choices) {
     }
     
     for ( let i = 0; i < choices.length; i++) {
+        // used the shuffleArray function to create the face cards and ammend them to the html DOM
         if (choices[i] === 'faceCard1')   { 
             const card1 = document.createElement('img')
             card1.src = 'images/faceCard.jpeg'
             card1.style.width = '150px'
             card1.alt = 'faceCard1'
-            card1.id = 'card1'
+            card1.id = 'faceCard1'
             document.getElementsByClassName('cards')[0].appendChild(card1) 
-            console.log(document.getElementsByClassName('cards')[0]);
+            console.log('in shuffleArray conditional card1',document.getElementsByClassName('cards')[0]);
       
         } else if (choices[i] === 'faceCard2') {  
             const card2 = document.createElement('img')
             card2.src = 'images/faceCard.jpeg'
             card2.style.width = '150px'
             card2.alt = 'faceCard2'
-            card2.id = 'card2'
+            card2.id = 'faceCard2'
             document.getElementsByClassName('cards')[0].appendChild(card2) 
-            console.log(document.getElementsByClassName('cards')[0]);
+            console.log('in shuffleArray conditional card2',document.getElementsByClassName('cards')[0]);
 
         } else  {  
             const card3 = document.createElement('img')
             card3.src = 'images/faceCard.jpeg'
             card3.style.width = '150px'
             card3.alt = 'faceCard3'
-            card3.id = 'card3'
+            card3.id = 'faceCard3'
             document.getElementsByClassName('cards')[0].appendChild(card3) 
-            console.log(document.getElementsByClassName('cards')[0]);
+            console.log('in shuffleArray conditional card3',document.getElementsByClassName('cards')[0]);
    
         }
-    console.log(choices)
+    console.log('in shuffleArray, choices',choices)
     }
+    
+
 }
+
 shuffleArray(choices);
 
+// const cardChoices = document.getElementsByClassName('cards')
+// const cardChoices = document.getElementsByClassName('cards')
+// const cards = document.getElementsByClassName('cards')
+
 const matchCheck = (event) => {
-    console.log(event.target.id)
+    console.log('in matchCheck, event.target.id',event.target.id)
     let selection = event.target.id
-    if (selection == choices[0]) {
+    console.log('matchCheck, choices', choices)
+    if (selection === 'faceCard1') {
         result = 'match';
        
     } else  {
         result = 'computers turn';
     } 
-    console.log(result)
+    console.log('in matchCheck, result',result)
 }
 
-document.getElementById('card1').addEventListener('click', matchCheck)
-document.getElementById('card2').addEventListener('click', matchCheck)  
-document.getElementById('card3').addEventListener('click', matchCheck)     
+
+   
+
+
+function computerChooses() {
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    computer.currentChoice = choices[randomIndex];
+}
+    
+   
+
+
+
+
+
+document.getElementById('faceCard1').addEventListener('click', matchCheck)
+document.getElementById('faceCard2').addEventListener('click', matchCheck)  
+document.getElementById('faceCard3').addEventListener('click', matchCheck)  
+// //////original click event listeners that were used as models for new event listeners////
+// document.getElementById('faceCard1').addEventListener('click', compareChoices);
+// document.getElementById('faceCard2').addEventListener('click', compareChoices);
+// document.getElementById('faceCard3').addEventListener('click', compareChoices);
+// document.querySelector('.restart-game').addEventListener('click', handleRestartGame);
+
+
+// })
 // const playersTurn = () => {
 //     // let result;
 //      selection = 'faceCard1'
@@ -115,24 +148,3 @@ document.getElementById('card3').addEventListener('click', matchCheck)
 //     } 
 //     console.log(result)
 //   };
-
-
-    
-   
-
-
-function computerChooses() {
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    computer.currentChoice = choices[randomIndex];
-}
-
-
-
-
-// document.getElementById('faceCard1').addEventListener('click', compareChoices);
-// document.getElementById('faceCard2').addEventListener('click', compareChoices);
-// document.getElementById('faceCard3').addEventListener('click', compareChoices);
-// document.querySelector('.restart-game').addEventListener('click', handleRestartGame);
-
-
-// })
