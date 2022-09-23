40e0bfd9ce3c37b2e00dbb2ed9a781b63c7f46bd
melanie silva <melsil11@hotmail.com>
2022-09-23 11:03:03 -0400

    fixed computer turn by calling the functions in the matchCheck funtion. added remove event listeners so player cannot click cards after turn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // document.addEventListener("DOMContentLoaded", function(event) {
 
+// the two players of the game
 const player = {
     currentChoice: null
 }
@@ -14,16 +15,8 @@ const matchCard = ['matchCard']
 const choices = ['faceCard1', 'faceCard2', 'faceCard3'];
 
 
-// console.log(getElementsByClassName('cards'))
-// const choice2 = document.createElement('div')
-//     choice2.setAttribute('id', 'fisher') 
-//     document.body.appendChild(choice2);
-    // const fisher = document.createElement('img');
-    // fisher.src = 'images/fisher.jpg';
-    // document.body.appendChild(fisher);
-
-    // choice2.appendChild(fisher)
-
+// this function is based on fisher yates algorithm, it will shuffle the array
+// so that each game the cards are in a different order it also creates the cards and displays them
 function shuffleArray(choices) {
 
     for (let i = choices.length - 1; i > 0; i--) {
@@ -70,38 +63,54 @@ function shuffleArray(choices) {
 
 shuffleArray(choices);
 
-// const cardChoices = document.getElementsByClassName('cards')
-// const cardChoices = document.getElementsByClassName('cards')
-// const cards = document.getElementsByClassName('cards')
-
+// this function creates the target event click and checks if it matched the match card. 
+let playerTurn = true
 const matchCheck = (event) => {
     console.log('in matchCheck, event.target.id',event.target.id)
     let selection = event.target.id
     console.log('matchCheck, choices', choices)
     if (selection === 'faceCard1') {
         result = document.getElementById("results").innerText='Congratulations, You Win! Press the button to play again.';
-    //    stop game
+        removeHandler()
+    //    need to stop game
     } else  {
+        console.log('got here')
+        playerTurn = false
+        console.log(playerTurn)
         result = document.getElementById("results").innerText='Sorry, no match. It is the computers turn.';
+       
     } 
     console.log('in matchCheck, result',result)
-    // const newChoices = choices.indexOf(selection)
+     // remove the card choice from the array for computers turn
     choices.splice(choices.indexOf(selection),1)
-    console.log(choices)
+    console.log(choices) 
+    if (playerTurn === false) {
+        computerChooses()
+        computerChoice()
+       
+    }
 
-    // remove the card choice from the array for computers turn
 }
 
+function removeHandler() {
+    document.getElementById('faceCard1').removeEventListener('click', matchCheck);
+    document.getElementById('faceCard2').removeEventListener('click', matchCheck);
+    document.getElementById('faceCard3').removeEventListener('click', matchCheck);
+}
 
+// this function is where the computer selects a random choice 
 function computerChooses() {
-    
+    console.log('computer chooses ran')
+    if (playerTurn === false) {
     const randomIndex = Math.floor(Math.random() * choices.length);
     computer.currentChoice = choices[randomIndex];
     console.log(randomIndex)
+    removeHandler()
+    }  
+} 
 
-}   
-
-function computerChoice(computer)  {
+// this function compares the computers choice to determine if its a win or draw
+function computerChoice()  {
 
     if(computer.currentChoice === 'faceCard1') {
         result = document.getElementById("results").innerText='The Computer Wins. Press the play again button to restart the game';
@@ -125,10 +134,13 @@ console.log('in computerChooses, result',computerChooses)
 
 
 
-
+// these are the click event listeners for the player and will determin the outcome in the matchCheck function
 document.getElementById('faceCard1').addEventListener('click', matchCheck)
 document.getElementById('faceCard2').addEventListener('click', matchCheck)  
 document.getElementById('faceCard3').addEventListener('click', matchCheck)  
+
+
+
 // //////original click event listeners that were used as models for new event listeners////
 // document.getElementById('faceCard1').addEventListener('click', compareChoices);
 // document.getElementById('faceCard2').addEventListener('click', compareChoices);
@@ -136,58 +148,12 @@ document.getElementById('faceCard3').addEventListener('click', matchCheck)
 // document.querySelector('.restart-game').addEventListener('click', handleRestartGame);
 
 
-// })
-// const playersTurn = () => {
-//     // let result;
-//      selection = 'faceCard1'
-//     if (selection == choices[0]) {
-//         result = 'match';
-//     } else {
-//         result = 'computers turn';
-//     } 
-//     console.log(result)
-//   };
+// console.log(getElementsByClassName('cards'))
+// const choice2 = document.createElement('div')
+//     choice2.setAttribute('id', 'fisher') 
+//     document.body.appendChild(choice2);
+    // const fisher = document.createElement('img');
+    // fisher.src = 'images/fisher.jpg';
+    // document.body.appendChild(fisher);
 
-
-// const playersTurn2 = () => {
-//     // let result;
-//      selection = 'faceCard2'
-//     if (selection == choices[1]) {
-//         result = 'match';
-//     } else {
-//         result = 'computers turn';
-//     } 
-//     console.log(result)
-//   };
-
-
-
-// const playersTurn3 = () => {
-//     // let result;
-//      selection = 'faceCard3'
-//     if (selection == choices[2]) {
-//         result = 'match';
-//     } else {
-//         result = 'computers turn';
-//     } 
-//     console.log(result)
-//   };
-
-// function computerChooses() {
-//     const randomIndex = Math.floor(Math.random() * choices.length);
-//     computer.currentChoice = choices[randomIndex];
-//     console.log(randomIndex)
-//     if(computer.currentChoice === 'faceCard1') {
-//         result = document.getElementById("results").innerText='The Computer Wins. Press the play again button to restart the game';
-    
-//     } else if {
-//         (computer.currentChoice === 'faceCard2')
-//         result = document.getElementById("results").innerText='No one matched the Card. It is a Draw. Press the play again button to restart the game';
-    
-//     } else {
-//         (computer.currentChoice === 'faceCard3')
-//         result = document.getElementById("results").innerText='No one matched the Card. It is a Draw. Press the play again button to restart the game';
-
-//     }
-// }
-// console.log('in computerChooses, result',computerChooses)
+    // choice2.appendChild(fisher)
